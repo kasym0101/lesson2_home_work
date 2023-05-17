6e8c5e885299f882c9000c4128c12c3df7040891
Kasym0101 <kasym.dzhorobaev@gmail.com>
2023-05-17 16:08:41 +0600

    first commit

---
+++ lesson2_home_work.dart
@@ -0,0 +1,74 @@
+void main() {
+  num a = 1;
+  num b = 2.5;
+  num c = 10;
+  num d = 17.3;
+  num e = 250;
+
+  int a1 = 12;
+  int b1 = 10;
+  int c1 = 7;
+  int d1 = 15;
+  int e1 = 20;
+
+  double a2 = 1.2;
+  double b2 = 5.5;
+  double c2 = 4.7;
+  double d2 = 78.8;
+  double e2 = 55.1;
+
+  String name1 = 'Kasym';
+  String name2 = 'Dzhorobaev';
+  String name3 = "Flutter";
+  String name4 = "Dart";
+  String name5 = 'itcBootcamp';
+
+  List<int> dart1 = [10, 20, 30];
+  List<String> dart2 = ['Kasym', 'Dzhorobaev'];
+  List dart3 = [10, 'Kasym', true, 3.3];
+  List dart4 = [123, "abc", 10.5];
+  List dart5 = [true, false];
+
+  Map user1 = {'name': 'Kasym', 'age': 28};
+  Map user2 = {"color": "red", 'color2': 'white'};
+  Map user3 = {'number': 1, "number2": 2};
+  Map user4 = {'length': 5.5, 'length2': 3.4};
+  Map user5 = {'carName': 'Lexus', 'color': 'black', 'price': 15000};
+
+  var f = '\u{1F645}';
+  var j = '\u{1F608}';
+  var k = '\u{1F60D}';
+  var l = '\u{1F642}';
+  var m = '\u{1F648}';
+
+  final f1;
+  f1 = 20;
+  final j1;
+  j1 = 10;
+  final k2;
+  k2 = 'Kasym';
+  final l2;
+  l2 = 'Dzhorobaev';
+  final m2;
+  m2 = true;
+
+  const n = 70;
+  const o = 'Dart';
+  const p = 'Flutter';
+  const q = 5.5;
+  const r = false;
+
+  dynamic qwerty = 'qwerty';
+  qwerty = 55;
+  qwerty = 5.5;
+  qwerty = "Kasym";
+  qwerty = {'age': 28};
+  qwerty = [1, 2, 3];
+
+  var qwerty1 = 'qwerty';
+  qwerty1 = 'Kasym';
+  print(qwerty1);
+  var qwerty2 = 10;
+  qwerty2 = 20;
+  print(qwerty2);
+}
